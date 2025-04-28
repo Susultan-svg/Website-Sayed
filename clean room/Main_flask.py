f7accac2c2371f34138239ce1a332045040d1edd
Sulsultan <s4yed.sult4n@gmail.com>
2025-04-28 10:37:40 +0700

    change
    change

--- clean room/Main_flask.py
+++ clean room/Main_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -7,24 +7,27 @@ app = Flask(__name__)
 def Homepage():
     return render_template('Homepage.html')
 
-@app.route('/AboutMe') 
+@app.route('/AboutMe')
 def About_Me():
     return render_template('About Me.html', title="Halaman About Me")
 
-@app.route('/LaguFavorit') 
+@app.route('/LaguFavorit')
 def Lagu():
-    return render_template('Lagu.html', title="Halaman Lagu")
+    return render_template('lagu.html', title="Halaman Lagu")
 
 @app.route('/Projects')
 def Projects():
-    return render_template('Projects.html', title="Halaman Projects")
+    return render_template('Projects.html', title="Halamat Projects")
 
 @app.route('/Contact')
 def Contact():
-    return render_template('Contact.html', title="Halaman Contact")
+    return render_template('Contact.html', title="Halamat Contact")
 
 @app.route('/kasih', methods=['POST'])
 def kasih():
     nama = request.form['nama']
     rate = request.form['rate']
     return f'Terima kasih {nama}, kamu mengasih rating {rate}'
+
+if __name__ == "__main__":
+    app.run(debug=True) #file hanya akan berjalan jika di run dari sini
