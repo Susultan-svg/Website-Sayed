b2644af8bb3814b068b7b82c4f793c16cbf66a09
Sulsultan <s4yed.sult4n@gmail.com>
2025-04-28 09:26:27 +0700

    Deploy to Vercel
    menhapus app.run(), mengformatkan folder untuk verce. +vercel.json +requierment.txt

---
+++ clean room/api/index.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/Homepage')
+def Homepage():
+    return render_template('Homepage.html')
+
+@app.route('/AboutMe')
+def About_Me():
+    return render_template('AboutMe.html', title="Halaman About Me")
+
+@app.route('/LaguFavorit')
+def Lagu():
+    return render_template('Lagu.html', title="Halaman Lagu")
+
+@app.route('/Projects')
+def Projects():
+    return render_template('Projects.html', title="Halaman Projects")
+
+@app.route('/Contact')
+def Contact():
+    return render_template('Contact.html', title="Halaman Contact")
+
+@app.route('/kasih', methods=['POST'])
+def kasih():
+    nama = request.form['nama']
+    rate = request.form['rate']
+    return f'Terima kasih {nama}, kamu memberi rating {rate}'
+
+# Tidak butuh app.run() disini
+# if __name__ == "__main__":
+#     app.run(debug=True) #file hanya akan berjalan jika di run dari sini
